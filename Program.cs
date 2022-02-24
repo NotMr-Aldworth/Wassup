3d94413a6f8213aaf7c4f062041aef91222d246c
Jackson Cowan <CowaJack474@GOTVDSB.CA>
2022-02-24 10:26:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = ("Damn right I like the life I live cause I went from negative to positive.");
+            string firstName = "Jackson";
+            string favMovie = "The Matrix";
+            favMovie = (favMovie.ToLower());
+            firstName = (firstName.ToLower());
+            Console.WriteLine($"Hey {firstName} have you ever watched the movie {favMovie}?");
+            favMovie = (favMovie.ToUpper());
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = (favMovie.Replace("A", "@"));
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+
+
+            quote = (quote.ToLower());
+            quote = (quote.Replace("a", " ")).Replace("e", " ").Replace("i", " ").Replace("o", " ").Replace("u", " ");
+            Console.WriteLine(quote);
+
+
+            Console.WriteLine(" ");
+            Console.Write("===========");
+            Console.WriteLine("     __v_ ");
+            Console.Write("I         I");
+            Console.WriteLine("    (____/{ ");
+            Console.WriteLine("I         I");
+           
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("I         I");
+            
+            Console.WriteLine("===========");
+
+            
+            Console.WriteLine("|         / ");
+            
+            Console.WriteLine("|        / ");
+            
+            Console.WriteLine("|       / ");
+            
+            Console.WriteLine("|      / ");
+            
+            Console.WriteLine("|     / ");
+            
+            Console.WriteLine("|    / ");
+            
+            Console.WriteLine("|   / ");
+            
+            Console.WriteLine("|  / ");
+            
+            Console.WriteLine("| / ");
+            
+            Console.WriteLine("|/ ");
+            
+            Console.ReadLine();
+        }
+    }
+}
